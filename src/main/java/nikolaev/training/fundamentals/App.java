626379f0d324c11d161d2633d499ad332c10b76c
Andrey Nikolaev <aan.nikol@gmail.com>
2019-06-17 09:45:50 +0300

    Project initialization

---
+++ src/main/java/nikolaev/training/fundamentals/App.java
@@ -0,0 +1,13 @@
+package nikolaev.training.fundamentals;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
