6ea73b3517bd1eac89b6281ebc3f95af4b74103a
Andrey Nikolaev <aan.nikol@gmail.com>
2019-06-17 12:06:00 +0300

    Added number input from keyboard

--- src/main/java/nikolaev/training/fundamentals/App.java
+++ src/main/java/nikolaev/training/fundamentals/App.java
@@ -1,13 +1,35 @@
 package nikolaev.training.fundamentals;
 
+import java.util.*;
+
+
 /**
- * Hello world!
+ * JAVA.FUNDAMENTALS
+ * Optional Task 3
+ * Ввести число от 1 до 12. Вывести на консоль название месяца, соответствующего данному числу.
+ * Осуществить проверку корректности ввода чисел.
  *
  */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Scanner in = new Scanner(System.in);
+        int num;
+        boolean correctNumber = false;
+
+        while (!correctNumber){
+            System.out.print("Input a number: ");
+            num = in.nextInt();
+            if (num>=1 && num<=12){
+                System.out.println("Your number is correct: " + num);
+                correctNumber = true;
+            } else {
+                System.out.println("Your number is not correct: " + num);
+            }
+        }
+
+        in.close();
+
     }
 }
