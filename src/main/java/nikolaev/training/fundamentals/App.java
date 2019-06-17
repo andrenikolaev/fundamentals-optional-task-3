2c19bd4939a690d9e2d79f9b8a08d0a0ec84d299
Andrey Nikolaev <aan.nikol@gmail.com>
2019-06-17 13:03:06 +0300

    Exception handling InputMismatchException

--- src/main/java/nikolaev/training/fundamentals/App.java
+++ src/main/java/nikolaev/training/fundamentals/App.java
@@ -16,17 +16,22 @@ public class App
     public static void main( String[] args )
     {
         Scanner in = new Scanner(System.in);
-        int num =0;
-        System.out.print("Input a number: ");
+        int num = 0;
 
-        num = in.nextInt();
-
-        while (num>12 || num<1){
-            System.out.print("Your number is not correct! \nInput a number: ");
+        System.out.print("Enter a int number: ");
+        try {
             num = in.nextInt();
+
+            while (num>12 || num<1){
+                System.out.print("Your number is not correct! \n" + "Enter a int number: ");
+                num = in.nextInt();
+            }
         }
-
-        in.close();
-
+        catch (InputMismatchException ex){
+            System.out.println("Expected value of type int!");
+        }
+        finally {
+            in.close();
+        }
     }
 }
